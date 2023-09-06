36ba980f26b62ed38243c727fc3825f7819611f6
dev_Tushar24 <dev@tusharupadhyay.com>
2023-09-06 21:28:32 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../service/food/food.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+foods:string[]= [];
+  constructor(private fs:FoodService){}
+
+  ngOnInit(): void {
+    
+this.foods = this.fs.getAll();
+
+  }
+
+}
